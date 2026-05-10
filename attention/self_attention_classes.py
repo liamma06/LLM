5853138909fe35fe1944b9754fcda960c89d5512
liamma06 <liam.jbr.ma@gmail.com>
2026-05-09 20:31:04 -0400

    self attention v2 with nn.linear

--- attention/self_attention_classes.py
+++ attention/self_attention_classes.py
@@ -1,17 +1,43 @@
 import torch 
 import torch.nn as nn
 
+#uses rand numbers for weight initializations
 class SelfAttention_v1(nn.Module):
     def __init__(self, d_in, d_out):
+        #adding everything from nn.Module to this class
         super().__init__()
+
+        #initializing the weight matrices for query, key and value
         self.W_query = nn.Parameter(torch.rand(d_in, d_out))
         self.W_key = nn.Parameter(torch.rand(d_in, d_out))
         self.W_value = nn.Parameter(torch.rand(d_in, d_out))
 
     def forward(self, x):
+        #embedding transformations
         keys = x @ self.W_key
         queries = x @ self.W_query
         values = x @ self.W_value
+
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+    
+
+#using nn.Linear for weight
+#nn.linear uses an optimized initiliaztion designed to keep gradient stable and more effective for training
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, x):
+        #matrix multiplication is done inside the linear layer, so just pass the input through
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
 
         attn_scores = queries @ keys.T
         attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
