def22800680394ab2411538905f90fa0461486a0
liamma06 <liam.jbr.ma@gmail.com>
2026-05-19 13:09:09 -0400

    value loss print

--- Pretraining-on-unlabeled-data/Pretraining.py
+++ Pretraining-on-unlabeled-data/Pretraining.py
@@ -82,7 +82,7 @@ def train_model_simple(model, train_loader, val_loader, optimizer, device, num_e
                 train_losses.append(train_loss)
                 val_losses.append(vall_loss)
                 track_tokens_seen.append(tokens_seen)
-                print(f"Ep: {epoch+1} (Step {global_step:06d}): " f"Train loss {train_loss: .3f}")
+                print(f"Ep: {epoch+1} (Step {global_step:06d}): " f"Train loss {train_loss: .3f}" f"Val loss {val_loss: .3f}")
 
         generate_and_print_sample(model, tokenizer, device, start_context)
 
